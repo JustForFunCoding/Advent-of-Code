66c82868a2e97b29c50f2307790c610ca935ad48
Adam Krídl <xkridl@fi.muni.cz>
2022-12-07 19:18:57 +0100

    Update input utilities

--- 2022/input_utils.py
+++ 2022/input_utils.py
@@ -2,13 +2,16 @@ def get_text(filename: str) -> str:
     return open(filename).read()
 
 
-def get_lines(filename: str) -> list[str]:
+def get_unstripped_lines(filename: str) -> list[str]:
     return open(filename).readlines()
 
 
+def get_lines(filename: str) -> list[str]:
+    return list(map(str.strip, get_unstripped_lines(filename)))
+
+
 def get_line_groups(filename: str) -> list[list[str]]:
-    return list(map(lambda s: s.split(),
-                    get_text(filename).strip().split('\n\n')))
+    return list(map(str.split, get_text(filename).strip().split('\n\n')))
 
 
 def get_numbers(filename: str) -> list[int]:
